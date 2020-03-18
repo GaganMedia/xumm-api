9541b3a4d823edbbf4605ea63601453c019f8c1d
GaganMedia <62342917+GaganMedia@users.noreply.github.com>
2020-03-18 16:24:33 +0100

    pl.js

---
+++ translations/pl.js
@@ -0,0 +1,61 @@
+module.exports = {
+  LANGUAGE_CODE: 'pl',
+  ERROR: `Błąd`,
+  ERROR_404_BODY: `Niestety nie można znaleźć żądanej strony.`,
+  ERROR_500_BODY: `Przepraszamy, w tej chwili nie można przetworzyć Twojego żądania.`,
+  LIPSUM: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin maximus sem ante, sit amet semper mauris venenatis non. Fusce posuere, libero ut volutpat ornare, massa ipsum hendrerit ipsum, sed finibus ipsum augue a nisl. Etiam at tempus ante, sit amet tristique leo. Vestibulum lobortis nisl a volutpat rhoncus. Ut facilisis ante a iaculis sollicitudin. Ut consectetur magna at libero vulputate, id pulvinar dui rutrum. Etiam facilisis finibus tincidunt.`,
+  BY_NAME: `XPRL Labs`,
+  XRPL_SIGNPLATFORM: `XRPL Signing Platform (BETA)`,
+  TITLE_SIGNTX: `Podpisz prośbę`,
+  ALREADY_SIGNED: `Już rozwiązane`,
+  PAYLOAD_RESOLVED: `To żądanie zostało już rozwiązane (podpisane lub odrzucone).`,
+  SCAN_WITH_APP: `Zeskanuj ten kod QR za pomocą <b class="font-logo">xumm</b>`,
+  PAYLOAD_NOT_FOUND: `Nie znaleziono prośby o podpis.`,
+  PAYLOAD_EXPIRED: `Zgłoszenie podpisu wygasło.`,
+  PAYLOAD_BACKEND_ERROR: `Przepraszamy, wystąpił błąd podczas pobierania danych do podpisu.`,
+  PROFILE_BACKEND_ERROR: `Przepraszamy, wystąpił błąd podczas pobierania danych profilu użytkownika.`,
+  PAYLOAD_PAGE_FOR_MOBILE: `Aby podpisać prośbę, upewnij się, że masz zainstalowaną aplikację <b class="font-logo">xumm</b>`,
+  APPLICATION_DISABLED: `Aplikacja <q><b>__appname__</b></q> jest wyłączona.`,
+  WAIT_CONNECT_PLATFORM: `Łączenie z platformą do podpisu.`,
+  WAIT_OPEN: `Oczekiwanie na otwarcie 
+                  <code class="text-primary"><b class="text-primary"><q>__txtype__</q></b></code>
+                  żądania w aplikacji XUMM.`,
+  WAIT_SCAN_QR: `Oczekiwanie na zeskanowanie 
+                  <code class="text-primary"><b class="text-primary"><q>__txtype__</q></b></code>
+                  żądania za pomocą aplikacji XUMM.`,
+  WAIT_SCAN_QR_DETAILED: `Oczekiwanie na skanowanie i otwarcie 
+                            <code class="text-primary"><b class="text-primary">__txtype__</b></code>
+                          do 
+                            <code class="text-primary"><b>__txdestination__</b></code>
+                          za pomocą aplikacji XUMM.`,
+  WAIT_FOR_RESOLVE: `Oczekiwanie na rozwiązanie (zaakceptowanie / odrzucenie) 
+                      <code class="text-primary"><b class="text-primary"><q>__txtype__</q></b></code>
+                      wniosku w aplikacji XUMM.`,
+  SHOW_TRANSACTION: `Pokaż prośbę o podpis`,
+  RESOLVED: `Żądanie podpisu zostało pomyślnie podpisane.`,
+  RESOLVED_NEXT_RETURN: `Powrócisz do <q><b>__appname__</b></q> za kilka sekund`,
+  REJECTED: `Żądanie podpisu zostało odrzucone.`,
+  REJECTED_NEXT_RETURN: `Powrócisz do <q><b>__appname__</b></q> za kilka sekund`,
+  GET_APP_IOS_TITLE: `Pobierz 𝘅𝘂𝗺𝗺 na iOS w Apple App Store`,
+  GET_APP_ANDROID_TITLE: `Pobierz 𝘅𝘂𝗺𝗺 na Androida w sklepie Google Play`,
+  MODAL_CLOSE: `Zamknij`,
+  GET_APP_IOS_MODAL_TITLE: `Pobierz <b class="font-logo">xumm</b> na iOS`,
+  GET_APP_ANDROID_MODAL_TITLE: `Pobierz <b class="font-logo">xumm</b> na Androida`,
+  GET_APP_IOS_MODAL_SCAN: `Zeskanuj kod QR za pomocą urządzenia z systemem iOS, aby odwiedzić <i class="fab fa-apple"></i> Apple App Store.`,
+  GET_APP_ANDROID_MODAL_SCAN: `Zeskanuj kod QR za pomocą urządzenia z Androidem, aby odwiedzić <i class="fab fa-google-play"></i> Sklep Google Play.`,
+  GET_APP_IOS_MODAL_VISIT: `Odwiedź Apple App Store`,
+  GET_APP_ANDROID_MODAL_VISIT: `Odwiedź sklep Google Play`,
+  PAYLOAD_PUSH_NOTIFICATION_SENT: `Powiadomienie push zostało wysłane na Twoje urządzenie(a) mobilne.`,
+  DIDNT_RECEIVE_PUSH_TEXT: `Nie otrzymałeś wiadomości push?`,
+  SHOW_QR: `Zeskanuj kod QR ponownie`,
+  OPEN_IN_XUMM: 'Otwórz w aplikacji <b class="font-logo">xumm</b>',
+  PLEASE_INSTALL_APP: `Proszę zainstaluj <b class="font-logo">xumm</b>`,
+  DASH_OR: '-lub-',
+  INSTALL_APP: `Zainstaluj <b class="font-logo">xumm</b>`,
+  TITLE_TX_SCANNED: `Otwarty w <b class="font-logo">xumm</b>`,
+  PAYLOAD_ALREADY_OPENED: `Żądanie podpisania zostało otwarte`,
+  NEED_QR_PAYLOAD_QR: `Możesz ponownie otworzyć żądanie podpisania. Żądanie podpisania wygasa <span class="ts-moment">__in_x_time_expire__</span>.`,
+  PUSH_MSG_SIGN_REQUEST: `Prośba logowania`,
+  PUSH_MSG_TXTYPE_TO_DEST: `__type__ do __destination__`,
+  PUSH_MSG_SIGNIN_REQ: `Zaloguj się do __appname__`
+}
